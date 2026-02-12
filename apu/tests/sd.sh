0e9b41196ff76e94079072286f713b769b1cbd25
Takumi Ando <takumi@spacecubics.com>
2026-02-12 21:00:48 +0900

    apu: Rename emmc.sh to sd.sh
    Signed-off-by: Takumi Ando <takumi@spacecubics.com>

--- apu/tests/sd.sh
+++ apu/tests/sd.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-DEV=/dev/mmcblk1
-MNT=mnt/emmc
+DEV=/dev/mmcblk0
+MNT=mnt/sd
 
 PASS_COUNT=0
 ERROR_COUNT=0
 
 pass () {
-    printf '\033[1;32meMMC:\033[0m %s (%d)\n' "$1" "$((++PASS_COUNT))"
+    printf '\033[1;32mSD:\033[0m %s (%d)\n' "$1" "$((++PASS_COUNT))"
 }
 
 error () {
-    printf '\033[1;31meMMC: %s (%d)\033[0m\n' "$1" "$((++ERROR_COUNT))"
+    printf '\033[1;31mSD: %s (%d)\033[0m\n' "$1" "$((++ERROR_COUNT))"
 }
 
 mkdir -p "$MNT"
@@ -30,23 +30,23 @@ do
         continue
     fi
 
-    dd if=/dev/urandom of=/tmp/emmctest.bin bs=1M count=50 2>/dev/null
+    dd if=/dev/urandom of=/tmp/sdtest.bin bs=1M count=50 2>/dev/null
 
-    if ! cp /tmp/emmctest.bin "$MNT/emmctest.bin" 2>/dev/null; then
+    if ! cp /tmp/sdtest.bin "$MNT/sdtest.bin" 2>/dev/null; then
         error "Failed to write to device"
         umount "$MNT" > /dev/null 2>&1
         sleep 1
         continue
     fi
 
-    if ! cmp /tmp/emmctest.bin "$MNT/emmctest.bin" > /dev/null 2>&1; then
+    if ! cmp /tmp/sdtest.bin "$MNT/sdtest.bin" > /dev/null 2>&1; then
         error "Data verification failed"
         umount "$MNT" > /dev/null 2>&1
         sleep 1
         continue
     fi
 
-    rm /tmp/emmctest.bin
+    rm /tmp/sdtest.bin
     umount "$MNT" > /dev/null 2>&1
     pass "Test successful"
 done
